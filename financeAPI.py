cac87fbe0e70cdd8c3b9a2381e10738f8a41f520
junho0327 <80371892+junho0327@users.noreply.github.com>
2022-06-23 16:43:39 +0900

    Update financeAPI.py

--- financeAPI.py
+++ financeAPI.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import json
 
-url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=KBlRL8EOIfKEDiYuevOAnKlE0ynDJUGa&searchdate=20180102&data=AP01"
+url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=&searchdate=20180102&data=AP01"
 responseBody = urlopen(url + "1").read().decode('utf-8')
 jsonArray = json.loads(responseBody)
 storeInfosArray = jsonArray.get("storeInfos")
